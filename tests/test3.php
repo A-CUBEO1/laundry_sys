85c19593ecab1790f6d287cdfc54aafcc8f2067d
G3OV3 <geogomboc@gmail.com>
2025-11-28 20:25:00 +0800

    Add files via upload

---
+++ tests/test3.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laundry Kiosk - Main</title>
+    <link rel="stylesheet" href="test3.css">
+</head>
+<body>
+    <div class="pg2-container">
+
+       
+        <aside class="pg2-sidebar">
+            <div class="pg2-sidebar-logo">Laundry Kiosk</div>
+            <ul class="pg2-sidebar-menu">
+                <li><a href="#">Main</a></li>
+                <li><a href="test2.php">Dashboard</a></li>
+                <li><a href="test3.php">Transactions</a></li>
+                <li><a href="#">Edit</a></li>
+                <li><a href="test0.php">Logout</a></li>
+            </ul>
+        </aside>
+
+    
+       <div class="pg2-main-content">
+    <header class="pg2-topbar">
+        <h1>Transaction</h1>
+        <div class="pg2-user-info">Admin/Cashier</div>
+    </header>
+
+    <div class="container">
+          <h1></h1>
+        </div> 
+    
+         
+ 
+</div>
+</body>
+</html>
