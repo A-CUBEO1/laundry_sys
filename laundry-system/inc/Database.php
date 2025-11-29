b872ea8ae0e3a02f43bfcce6b6883618af56287b
A-CUBEO1 <acubeo1wastaken@gmail.com>
2025-11-30 05:32:47 +0800

    Add files via upload

---
+++ laundry-system/inc/Database.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+    public $pdo;
+
+    public function __construct(){
+        $host = 'localhost';
+        $db   = 'laundry_system';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+
+        $this->pdo = new PDO($dsn, $user, $pass, $options);
+    }
+}
+?>
